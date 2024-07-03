ad83eff91e1170af73ca975e29b0f30fc130c2fc
Lakvidu Rathnayake <157009017+LakviduUpasara@users.noreply.github.com>
2024-07-03 22:35:46 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+	// varibale creation
+	int Num1 ,Num2 , total;
+	
+	
+	// get user input 
+	
+	printf("Enter Number :");
+	scanf("%d",&Num1) ;
+	
+	printf("Enter Number :");
+	scanf("%d",&Num2) ;
+	
+	total =Num1 +Num2 ;
+	
+	printf("%s","invalid Number") ;
+	
+	
+	
+  
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
---
+++ Untitled3.cpp
@@ -0,0 +1,95 @@
+#include<stdio.h>
+
+int main ()
+{
+	// creat variable 
+	
+	int sawanType ,NumberOfSawan  ;
+	char oderType , nextOder;
+	
+	float total ;
+	
+	//get User Input
+	
+	do {
+		
+			printf("Please Enter Sawan Type : ") ;
+			scanf("%d",&sawanType) ;
+			
+			printf("Please Enter Number Of Sawan :");
+			scanf("%d",&NumberOfSawan) ;
+			
+			printf("Please Enter oder Type ");
+			scanf(" %c",&oderType); 
+			
+			
+		
+			
+			if (sawanType == 1 )
+			{
+				total = NumberOfSawan * 10000.00 ;
+			
+				
+				
+			}else if (sawanType == 2)
+			{
+				total = NumberOfSawan * 12000.00 ;
+				
+			}else if (sawanType == 3)
+			{
+				total = NumberOfSawan * 15000.00 ;
+			}
+			else {
+				
+				printf("please Enter Valid Sawan Type \n") ;
+			}
+			
+			
+			if (oderType == 'I')
+			{
+				total =  (total *15 /100) + total ;
+				
+			}else if(oderType = 'T')
+			{
+				total =  (total *5 /100) + total ;
+				
+			}else if (oderType == 'D')
+			{
+				
+				total =  (total *5 /100) + total ;
+			}
+			
+			printf("\nThe Ammount to be Paid : %.2f", total) ;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	
+			printf("\nDo you want to Input the Next Oder Deatail : ") ;
+			scanf(" %c",&nextOder) ;
+			
+			//printf("%d",NumberOfSawan) ;
+			
+			printf("\n");
+			printf("\n");
+		
+			
+	
+	} while (nextOder != 'N') ;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
